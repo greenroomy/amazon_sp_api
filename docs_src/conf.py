69ff31289a8c328b00d75d5ff97ce53af098dc56
macm1 <greenroom1973@gmail.com>
2024-04-08 15:12:03 +0900

    add docs

--- docs_src/conf.py
+++ docs_src/conf.py
@@ -2,34 +2,33 @@ import os
 import sys
 import sp_lib
 import sphinx_rtd_theme
+from recommonmark.parser import CommonMarkParser
+from recommonmark.transform import AutoStructify
+
 
 sys.path.insert(0, os.path.abspath(''))
 release = sp_lib.__version__
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'amazon_sp_api: sp_lib'
 copyright = '2024, Shinji Uetsuki'
 author = 'Shinji Uetsuki'
 release = '1.0.0'
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
-
 templates_path = ['_templates']
 exclude_patterns = []
-
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+source_suffix = ['.rst', '.md']
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+github_doc_root = 'https://github.com/rtfd/recommonmark/tree/master/doc/'
+def setup(app):
+    app.add_config_value('recommonmark_config', {
+            'url_resolver': lambda url: github_doc_root + url,
+            'auto_toc_tree_section': 'Contents',
+            }, True)
+    app.add_transform(AutoStructify)
 
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
